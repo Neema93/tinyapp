257ffc4616776faede1aabefb14ca6f8dee1fdb8
Neema <patelneema93@gmail.com>
2021-11-08 17:42:06 -0500

    project working

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 8002; // default port 8080
+const PORT = 8003; // default port 8080
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -14,6 +14,10 @@ app.get("/urls.json", (req, res) => {
 });
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
 });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
