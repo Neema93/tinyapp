7729b3575c9f5a146e0db7b75c684b62103ca12f
Neema <patelneema93@gmail.com>
2021-11-08 20:47:07 -0500

    project work

--- express_server.js
+++ express_server.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const app = express();
-const PORT = 8005; // default port 8080
+const PORT = 8008; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+function generateRandomString() {
+
+}
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -20,14 +25,22 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: 'Hello World!' };
   res.render("hello_world", templateVars);
 });
+app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL:` http://localhost:8080/urls/b2xVn2` };
   res.render("urls_show", templateVars);
 });
+
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
 });
+app.post("/urls", (req, res) => {
+    console.log(req.body);  // Log the POST request body to the console
+    res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  });
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
